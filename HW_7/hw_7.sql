581136ef0466c55c14a726bd8f232234f3228806
Olyvia <73665865+Seila2009@users.noreply.github.com>
2021-02-23 01:39:00 +0300

    Update hw_7.sql

--- HW_7/hw_7.sql
+++ HW_7/hw_7.sql
@@ -10,3 +10,42 @@ SELECT products.name AS product_name, catalogs.name AS product_type
 -- (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). 
 -- Поля from, to и label содержат английские названия городов, поле name — русское. 
 -- Выведите список рейсов flights с русскими названиями городов.
+
+CREATE DATABASE IF NOT EXISTS flights_cities;
+
+USE flights_cities;
+
+DROP TABLE IF EXISTS flights;
+CREATE TABLE IF NOT EXISTS flights (
+	id SERIAL PRIMARY KEY,
+	`from` VARCHAR(255) COMMENT 'Город отправления',
+	`to` VARCHAR(255) COMMENT 'Город прибытия'
+) COMMENT = 'Рейсы';
+
+INSERT INTO flights (`from`, `to`) VALUES
+	('moscow', 'omsk'),
+	('nowgorod', 'kazan'),
+	('irkutsk', 'moscow'),
+	('omsk', 'irkutsk'),
+	('moscow', 'kazan');
+	
+DROP TABLE IF EXISTS cities;
+CREATE TABLE IF NOT EXISTS cities (
+	id SERIAL PRIMARY KEY,
+	label VARCHAR(255) COMMENT 'Код города',
+	name VARCHAR(255) COMMENT 'Название города'
+) COMMENT = 'Словарь городов';
+
+INSERT INTO cities (label, name) VALUES
+	('moscow', 'Москва'),
+	('irkutsk', 'Иркутск'),
+	('nowgorod', 'Новгород'),
+	('kazan', 'Казань'),
+	('omsk', 'Омск');
+	
+SELECT
+	id,
+	(SELECT name FROM cities WHERE label = flights.from) AS `from`,
+	(SELECT name FROM cities WHERE label = flights.to) AS `to`
+FROM
+	flights;
