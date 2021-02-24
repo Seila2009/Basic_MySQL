9c63a9630411e96811e2cf50fa2032ee48b37841
Olyvia <73665865+Seila2009@users.noreply.github.com>
2021-02-24 06:05:07 +0300

    Update hw_5.sql

--- HW_5/hw_5.sql
+++ HW_5/hw_5.sql
@@ -1,15 +1,10 @@
 -- 1. 
 UPDATE users SET created_at = NOW(), updated_at = NOW() WHERE created_at IS NULL OR updated_at IS NULL;
 
--- 2.Таблица users была неудачно спроектирована. Записи created_at и updated_at были заданы типом VARCHAR и в них долгое время помещались значения в формате 20.10.2017 8:10. 
--- Необходимо преобразовать поля к типу DATETIME, сохранив введённые ранее значения.
-
+-- 2.
 UPDATE users SET created_a = STR_TO_DATE(created_at, '%d.%m.%y %h:%i'), updated_at = STR_TO_DATE(updated_at, '%d.%m.%y %h:%i');
 
--- В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры: 0, если товар закончился и выше нуля, если на складе имеются запасы. 
--- Необходимо отсортировать записи таким образом, чтобы они выводились в порядке увеличения значения value. 
--- Однако нулевые запасы должны выводиться в конце, после всех 
-
+-- 3. 
 SELECT * FROM storehouses_products;
 
 INSERT INTO storehouses_products
@@ -19,20 +14,29 @@ INSERT INTO storehouses_products
 	(3, 3, 5, now(), now()),
 	(4, 4, 0, now(), now()),
 	(5, 5, 4, now(), now()),
-	(6, 6, 3, now(), now())
-;
+	(6, 6, 3, now(), now());
 
 SELECT * FROM storehouses_products ORDER BY CASE WHEN value = 0 THEN 9999999999999999999999 ELSE value END;
 
 -- (по желанию) Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае. 
 -- Месяцы заданы в виде списка английских названий (may, august)
-
 SELECT * FROM users WHERE birthday_at RLIKE '^[0-9]{4}-(05|08)-[0-9]{2}';
+
+-- (по желанию)Из таблицы catalogs извлекаются записи при помощи запроса. SELECT * FROM catalogs WHERE id IN (5, 1, 2); 
+-- Отсортируйте записи в порядке, заданном в списке IN.
+SELECT *, FIELD(`id`, 5, 1, 2) FROM catalogs WHERE id IN (5, 1, 2);
 
 -- Подсчитайте средний возраст пользователей в таблице users.
 
-SELECT AVG(age) FROM (SELECT YEAR(CURRENT_TIMESTAMP) - YEAR(birthday) as age FROM profiles) AS Avg_age;
+SELECT AVG(age) FROM (SELECT YEAR(CURRENT_TIMESTAMP) - YEAR(birthday_at) as age FROM `users`) AS Avg_age;
+-- второй вариант
+SELECT AVG(TIMESTAMPDIFF(YEAR, birthday_at, NOW())) FROM `users`;
 
--- Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. Следует учесть, что необходимы дни недели текущего года, а не года рождения.
-
-SELECT COUNT(*) as stats from (SELECT DAYOFWEEK(CONCAT(YEAR(NOW()),'-',MONTH(birthday),'-',DAYOFMONTH(birthday))) as date from profiles) as stats where date=1;
+-- Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. 
+-- Следует учесть, что необходимы дни недели текущего года, а не года рождения.
+SELECT 
+	DAYNAME(CONCAT(YEAR(NOW()), '-', MONTH(birthday_at), '-', DAY(birthday_at))) AS day_name, 
+    COUNT(*)
+FROM shop.users;
+GROUP BY day_name;
+ORDER BY WEEKDAY(CONCAT(YEAR(NOW()), '-', MONTH(birthday_at), '-', DAY(birthday_at))); 
